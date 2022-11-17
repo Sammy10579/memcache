8828c18b4829c955809d81846b4cbd3277a887d9
Sammy10579 <ramenskiivlad@gmail.com>
2022-11-17 13:04:36 +0300

    add client and docker memcache

---
+++ cmd/client/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/joho/godotenv"
+	"google.golang.org/grpc"
+	"log"
+	cache "memcache/api/proto"
+	"os"
+	"time"
+)
+
+func main() {
+
+	if err := godotenv.Load(); err != nil {
+		log.Print("No .env file found")
+	}
+
+	conn, err := grpc.Dial(os.Getenv("PORT"), grpc.WithInsecure())
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	c := cache.NewCacheClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	fmt.Println(c.Set(ctx, &cache.Item{Key: "123", Value: "dfgjkng"}))
+
+	fmt.Println("-----")
+
+	fmt.Println(c.Get(ctx, &cache.Key{Key: "123"}))
+}
